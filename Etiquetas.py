6cda5d618c53a0bf2da1d1e75c31a1a8459b26ac
Agustin G <agusgibaut693@gmail.com>
2025-11-01 18:44:11 +0100

    Tkinder Manual

---
+++ Etiquetas.py
@@ -0,0 +1,24 @@
+#TKINDER (IMPORTADO)
+import tkinter as tk
+#MODULO TTK (es una mejora de la libreria de tkinder.)
+from tkinter import ttk
+
+#CONFIGURACION INICIAL
+ventana = tk.Tk()
+ventana.geometry("600x400")
+ventana.title("REPRODUCTOR MUSICAL")
+
+#CREAMOS ETIQUETA
+etiqueta = tk.Label(ventana, text="HOLA, ACA SE MUESTRA EL TEXTO")
+
+#PUBLICAMOS EL COMPONENTES
+etiqueta.pack(pady=50)
+
+#tambien se puede cambiar el texto usando configure
+etiqueta.configure(text="Texto Modificado...")
+
+#Tambien, se pude modificar el texto con la ayuda de la llave Text
+etiqueta["text"] = "TAEXTO QUE SE PASO A MAYUSCULAS"
+
+#IMPORTANTE PARA PODER EJECUTAR LA VENTANA DE LA APLICACION.
+ventana.mainloop()
---
+++ Tkinder.py
@@ -0,0 +1,20 @@
+#IMPORTAR TKINDER.
+import tkinter as tk
+
+#AHORA CREAR LA INTERFAZ DE TKINDER.
+ventana = tk.Tk()
+
+#REDIMENCIONAR LA VENTANA.
+ventana.geometry("600x400")
+
+#MODIFICAR EL TITULO DE LA VENTANA.
+ventana.title("REPRODUCTOR MUSICAL")
+
+#EVITAR REDIMENSIONAR LA VENTANA
+ventana.resizable(0,0)
+
+#HACER VISIBLE LA VENTANA.
+ventana.mainloop()
+
+#MODIFICAR EL COLOR DE LA VENTANA
+ventana.configure(background="#fffffff")
